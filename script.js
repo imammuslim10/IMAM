3e3d0f8e4a1824590dbfc63b46140d2c7a92de0d
imammuslim10 <imammuslim6004@gmail.com>
2025-07-12 21:14:24 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("color-button").addEventListener("click", function() {
-  const colors = ["#ffcccc", "#ccffcc", "#ccccff", "#ffffcc"];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
-});
